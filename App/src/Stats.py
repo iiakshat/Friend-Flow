8b95f42aa59aba26681e5f99a88e3889b831890b
Akshat Sanghvi <jakshat569@gmail.com>
2024-01-12 13:12:46 +0530

    Update file

--- App/src/Stats.py
+++ App/src/Stats.py
@@ -1,15 +1,26 @@
-def messages(user, df):
+cache = {}
+
+
+def counter(user, df):
+    global cache
+    if user in cache:
+        return cache[user]
+    
     if user:
         df2 = df[df['user']==user]
         msgs = df2.shape[0]
         words = []
         for sentence in df2['message']:
             words.extend(sentence.split())
-        return msgs, len(words)
+        media = sum(df2['media'])
+        cache[user] = (msgs, len(words), media)
 
     else:
         msgs = df.shape[0]
         words = []
+        media = sum(df['media']) 
         for sentence in df['message']:
             words.extend(sentence.split())
-        return msgs, len(words)
+        cache[user] = (msgs, len(words),media)
+    
+    return cache[user]
