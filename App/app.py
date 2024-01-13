5658bf11a649d63f0852dbd7846503e5511fd122
Akshat Sanghvi <jakshat569@gmail.com>
2024-01-13 22:15:15 +0530

    Added graph1

--- App/app.py
+++ App/app.py
@@ -3,7 +3,7 @@ from werkzeug.utils import secure_filename
 import secrets
 import os
 from src.Preprocessor import preprocess
-from src.Stats import counter
+from src.Stats import counter, most_busy_users
 
 
 
@@ -63,15 +63,28 @@ def analyze(filepath):
 def perform_analysis():
     global df
     selected_user = request.form.get("user")
+    unique_users = df['user'].unique().tolist()
     if selected_user == 'all':
         selected_user_display = "All Users"
         msgs, words, media = counter(None, df)
     else:
         selected_user_display = selected_user
         msgs, words, media = counter(selected_user, df)
+    
+    graph_html, busiest, x = most_busy_users(selected_user, df)
+    if len(unique_users)>3:
+        if busiest == selected_user or selected_user=='Zuckerberg' or selected_user=='all':
+            busiest = busiest + ' did highest number of Messages.'
+        else:
+            busiest = busiest + f''' did highest number of Messages & {selected_user} did {x}% Messages.'''
+
+    else:
+        busiest = busiest + ' did more Messages.'
 
     delete_saved_file()
-    return render_template('analyze.html', unique_users=df['user'].unique().tolist(), results={'msgs': msgs, 'words': words, 'media': media}, selected_user=selected_user_display)
+    return render_template('analyze.html', unique_users=unique_users, 
+                           results={'msgs': msgs, 'words': words, 'media': media}, 
+                           selected_user=selected_user_display, graph_html=graph_html, busiest=busiest)
 
 
 def delete_saved_file():
--- App/src/Stats.py
+++ App/src/Stats.py
@@ -1,6 +1,10 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import base64
+from io import BytesIO
+
 cache = {}
-
-
 def counter(user, df):
     global cache
     if user in cache:
@@ -24,3 +28,48 @@ def counter(user, df):
         cache[user] = (msgs, len(words),media)
     
     return cache[user]
+
+def most_busy_users(suser, df):
+    data = round((df['user'].value_counts()/df.shape[0])*100,2)
+    data.drop("Zuckerberg",inplace=True)
+    x = ''
+
+    if len(df['user'].unique()) < 4:
+        ff = data
+        name = ff.index.tolist()
+        val = ff.tolist()
+        explode = [0.07, 0]
+        colors = ['#D2E0FB', '#8EACCD']
+
+    else:
+        x = data.get(suser)
+        ff = data.head(5)
+        name = ff.index.tolist()
+        val = ff.tolist()
+
+        rem = 100-sum(val)
+        explode = [0.07, 0, 0, 0, 0, 0]
+        
+        name.append('Others')
+        val.append(rem)
+        colors = ['#ff9999','#66b3ff','#99ff99','#ffcc99', '#9376e0', '#ff74b1']
+
+    fig1, ax1 = plt.subplots()
+    fig1.set_facecolor('None') 
+    ax1.pie(val, colors = colors, labels=name, autopct='%1.1f%%', 
+            startangle=90, pctdistance=0.85, explode = explode)
+    centre_circle = plt.Circle((0,0),0.70,fc='#DEF5E5')
+
+    fig = plt.gcf()
+    fig.gca().add_artist(centre_circle)
+    ax1.axis('equal')  
+    plt.tight_layout()
+
+    img_data = BytesIO()
+    plt.savefig(img_data, format='png')
+    img_data.seek(0)
+    encoded_img = base64.b64encode(img_data.getvalue()).decode('utf-8')
+    img_html = f'<img src="data:image/png;base64,{encoded_img}" alt="Matplotlib Graph">'
+    
+    return img_html, name[0], x
+    
