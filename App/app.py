ce57203357c87430cd4859c8db3d49364432f8e6
Akshat Sanghvi <jakshat569@gmail.com>
2024-02-08 14:47:12 +0530

    Update app.py

--- App/app.py
+++ App/app.py
@@ -150,4 +150,4 @@ def delete_saved_file():
             flash(f"Error deleting file: {str(e)}")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=80)
